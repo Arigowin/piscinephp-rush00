6583065777d0efd6d12dce7125e5ecd15a44b0cc
Martin NICOLAS <mnicolas@e3r11p13.42.fr>
2016-06-19 11:45:42 +0200

    front begins

--- index.php
+++ index.php
@@ -5,12 +5,14 @@
 
 <html>
   <head>
+    <link rel="stylesheet" type="text/css" href="index.css" />
+    <meta charset="utf-8">
     <title>Accueil </title>
+    <h1 class="home">This is Tennis</h1>
   </head>
 
-  <body>
-    <form method="link" action="gestion_log/create.html">
-      <input type="submit" value="Nouveau compte"></input>
+  <body class="fond">
+
     </form>
     <?php if ($_SESSION['error'] !== "")
     {
@@ -18,22 +20,28 @@
       error_msg(-1);
     }
     ?>
+        <div class="boxright">
     <?php if (isset($_SESSION['login']) && $_SESSION['login'] !== "") {?>
-    <form action="gestion_log/logout.php">
-        <input type="submit" value="Se deconnecter"></input>
-    </form>
-    <form action="gestion_log/del_account.php">
-        <input type="submit" value="Supprimer mon compte"></input>
-    </form>
-    <form action="gestion_log/modif.html">
-      <input type="submit" value="Modifier mon mot de passe"></input>
-    </form>
-    <?php }
-    else {?>
-      <form action="gestion_log/login.html">
-          <input type="submit" value="Se connecter"></input>
+
+      <p class="login"> Bonjour <?php if (isset($_SESSION['login'])) echo $_SESSION['login'];?></p>
+      <form action="gestion_log/logout.php">
+          <center><input class="btn" type="submit" value="Se déconnecter"></input></center>
       </form>
+      <form action="gestion_log/del_account.php">
+          <center><input class="btn" type="submit" value="Supprimer mon compte"></input></center>
+      </form>
+      <form action="gestion_log/modif.html">
+        <center><input class="modif" type="submit" value="Modifier mon mot de passe"></input></center>
+      </form>
+      <?php }
+        else {?>
+          <form action="gestion_log/login.html">
+            <center><input class="btn" type="submit" value="Se connecter"></input></center>
+          </form>
+          <form method="link" action="gestion_log/create.html">
+            <center><input class="btn" type="submit" value="Nouveau compte"></input></center>
+
       <?php } ?>
-    <p> <?php if (isset($_SESSION['login'])) echo $_SESSION['login'];?></p>
+  </div>
   </body>
 </html>
