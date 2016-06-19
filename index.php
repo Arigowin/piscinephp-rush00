8d64d23ac0b5725e5be23ebc1fc2f1d309e23e19
Martin NICOLAS <mnicolas@e3r11p13.42.fr>
2016-06-19 13:49:10 +0200

    front suite

--- index.php
+++ index.php
@@ -24,16 +24,23 @@
     <?php if (isset($_SESSION['login']) && $_SESSION['login'] !== "") {?>
 
       <p class="login"> Bonjour <?php if (isset($_SESSION['login'])) echo $_SESSION['login'];?></p>
-      <form action="gestion_log/logout.php">
+  <!--  <form action="gestion_log/logout.php">
           <center><input class="btn" type="submit" value="Se déconnecter"></input></center>
-      </form>
-      <form action="gestion_log/del_account.php">
+      </form>-->
+      <a href="gestion_log/logout.php"><button class="btn" type="submit" >Se déconnecter</button></a>
+<!--      <form action="gestion_log/del_account.php">
           <center><input class="btn" type="submit" value="Supprimer mon compte"></input></center>
-      </form>
-      <form action="gestion_log/modif.html">
+      </form>-->
+        <a href="gestion_log/del_account.php"><button class="btn" type="submit" >Supprimer mon compte</button></a>
+  <!--    <form action="gestion_log/modif.html">
         <center><input class="modif" type="submit" value="Modifier mon mot de passe"></input></center>
-      </form>
-      <?php }
+      </form>-->
+        <a href="gestion_log/modif.php"><button class="btn" type="submit" >Modifier mon mot de passe</button></a>
+
+      <?php if ($_SESSION['admin'] == FALSE) { ?>
+      <a href="gestion_admin.php"><button class="btn" type="submit" >Accéder à la page Administrateur</button></a>
+<?php }
+    }
         else {?>
           <form action="gestion_log/login.html">
             <center><input class="btn" type="submit" value="Se connecter"></input></center>
@@ -42,6 +49,14 @@
             <center><input class="btn" type="submit" value="Nouveau compte"></input></center>
 
       <?php } ?>
+  </div>
+  <div class="background">
+  <div class="transbox">
+    <p>This is some text that is placed in the transparent box.</p>
+  </div>
+</div>
+  <div class="boxleft">
+    <a href="articles.php" class="link">Tout voir</a>
   </div>
   </body>
 </html>
